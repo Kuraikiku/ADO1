7b5c4dddcb497614ab77f45ff78d124a02c0bcc5
Kuraikiku <LudwigFabian@dv-schulen.de>
2017-03-23 13:59:21 +0100

    Projektdateien hinzufügen.

---
+++ ado/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConn = new System.Windows.Forms.Button();
+            this.buttonSql = new System.Windows.Forms.Button();
+            this.buttonAuslesen = new System.Windows.Forms.Button();
+            this.listBoxAusgabe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonConn
+            // 
+            this.buttonConn.Location = new System.Drawing.Point(25, 22);
+            this.buttonConn.Name = "buttonConn";
+            this.buttonConn.Size = new System.Drawing.Size(208, 29);
+            this.buttonConn.TabIndex = 0;
+            this.buttonConn.Text = "Connection öffnen";
+            this.buttonConn.UseVisualStyleBackColor = true;
+            this.buttonConn.Click += new System.EventHandler(this.buttonConn_Click);
+            // 
+            // buttonSql
+            // 
+            this.buttonSql.Enabled = false;
+            this.buttonSql.Location = new System.Drawing.Point(25, 57);
+            this.buttonSql.Name = "buttonSql";
+            this.buttonSql.Size = new System.Drawing.Size(208, 29);
+            this.buttonSql.TabIndex = 1;
+            this.buttonSql.Text = "SQL - Command";
+            this.buttonSql.UseVisualStyleBackColor = true;
+            this.buttonSql.Click += new System.EventHandler(this.buttonSql_Click);
+            // 
+            // buttonAuslesen
+            // 
+            this.buttonAuslesen.Enabled = false;
+            this.buttonAuslesen.Location = new System.Drawing.Point(25, 92);
+            this.buttonAuslesen.Name = "buttonAuslesen";
+            this.buttonAuslesen.Size = new System.Drawing.Size(208, 29);
+            this.buttonAuslesen.TabIndex = 2;
+            this.buttonAuslesen.Text = "auslesen";
+            this.buttonAuslesen.UseVisualStyleBackColor = true;
+            this.buttonAuslesen.Click += new System.EventHandler(this.buttonAuslesen_Click);
+            // 
+            // listBoxAusgabe
+            // 
+            this.listBoxAusgabe.FormattingEnabled = true;
+            this.listBoxAusgabe.Location = new System.Drawing.Point(25, 139);
+            this.listBoxAusgabe.Name = "listBoxAusgabe";
+            this.listBoxAusgabe.Size = new System.Drawing.Size(208, 186);
+            this.listBoxAusgabe.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 346);
+            this.Controls.Add(this.listBoxAusgabe);
+            this.Controls.Add(this.buttonAuslesen);
+            this.Controls.Add(this.buttonSql);
+            this.Controls.Add(this.buttonConn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConn;
+        private System.Windows.Forms.Button buttonSql;
+        private System.Windows.Forms.Button buttonAuslesen;
+        private System.Windows.Forms.ListBox listBoxAusgabe;
+    }
+}
+
---
+++ ado/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ado
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = null;
+        OleDbCommand cmd = null;
+        OleDbDataReader reader = null;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void buttonConn_Click(object sender, EventArgs e)
+        {
+            OleDbConnectionStringBuilder bldr = new OleDbConnectionStringBuilder();
+            bldr.Provider = "Microsoft.ACE.OLEDB.12.0";
+            bldr.DataSource = "Bestellung.accdb";
+            con = new OleDbConnection(bldr.ConnectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message,"Exception",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+            buttonSql.Enabled = true;
+            buttonConn.Enabled = false;
+        }
+
+        private void buttonSql_Click(object sender, EventArgs e)
+        {
+            cmd = con.CreateCommand();
+            cmd.CommandText = "Select * from tArtikel";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message,"Omgush",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            buttonAuslesen.Enabled = true; ;
+            
+        }
+
+        private void buttonAuslesen_Click(object sender, EventArgs e)
+        {
+            while(reader.Read())
+            {
+                listBoxAusgabe.Items.Add(reader["ArtikelOid"].ToString() + " °^° " + reader["Bezeichnung"].ToString());
+            }
+        }
+    }
+}
